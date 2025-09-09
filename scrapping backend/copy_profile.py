99027546302e864a00b4c43928dcae60057cec1e
abhayshrivastava1 <abhay182981@gmail.com>
2025-09-09 15:21:59 +0530

    correction

--- scrapping backend/copy_profile.py
+++ scrapping backend/copy_profile.py
@@ -6,13 +6,13 @@ destination_profile = r"D:\whatsapp-bot-profile"
 
 # Make sure destination directory is clean
 if os.path.exists(destination_profile):
-    print(f"⚠️ Destination '{destination_profile}' already exists. Removing it...")
+    print(f" Destination '{destination_profile}' already exists. Removing it")
     shutil.rmtree(destination_profile)
 
-print(f"📁 Copying Chrome Profile from:\n{source_profile}\n→\n{destination_profile}")
+print(f" Copying Chrome Profile from:\n{source_profile}\n→\n{destination_profile}")
 
 try:
     shutil.copytree(source_profile, destination_profile)
-    print("✅ Profile cloned successfully!")
+    print(" Profile cloned successfully!")
 except Exception as e:
-    print(f"❌ Error cloning profile:\n{e}")
+    print(f" Error cloning profile:\n{e}")
